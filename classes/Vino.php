bf9c1480d062e853caaf1691c6588b065fa73881
Stefano Riccio <riccio.stefano0@gmail.com>
2020-03-10 17:50:25 +0100

    spostati i file e fatto include

--- classes/Vino.php
+++ classes/Vino.php
@@ -37,30 +37,4 @@ class Vini {
 
 }
 
-
-
-$barbera = new Vini('Cascina la Barbatella');
-$barbera->annata = '2016';
-$barbera->tipologia = 'Rosso';
-$barbera ->alcol = '14%';
-
-
-$gewurztrminer = new VINI('Elena Walch');
-$gewurztrminer->tipologia = 'Bianco';
-$gewurztrminer->annata = 2018;
-$gewurztrminer->ultimoConsumo = 2022;
-$gewurztrminer->alcol = '15%';
-echo $gewurztrminer->consumoIdeale() . 'anni';
-
-
-
-$listaVini = [
-  $barbera,
-  $gewurztrminer
-];
-
-foreach ($listaVini as $vino) {
-  //echo $vino->annata . '   ';
-}
-
 ?>
---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+include 'classes/Vino.php'
+
+$barbera = new Vini('Cascina la Barbatella');
+$barbera->annata = '2016';
+$barbera->tipologia = 'Rosso';
+$barbera ->alcol = '14%';
+
+
+$gewurztrminer = new VINI('Elena Walch');
+$gewurztrminer->tipologia = 'Bianco';
+$gewurztrminer->annata = 2018;
+$gewurztrminer->ultimoConsumo = 2022;
+$gewurztrminer->alcol = '15%';
+echo $gewurztrminer->consumoIdeale() . 'anni';
+
+
+
+$listaVini = [
+  $barbera,
+  $gewurztrminer
+];
+
+foreach ($listaVini as $vino) {
+  //echo $vino->annata . '   ';
+}
+?>
